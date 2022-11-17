59868983ececf67076e9d84f904ce10dbd67622c
William Trolinger <77353770+willtrolinger@users.noreply.github.com>
2022-11-17 17:55:54 -0600

    sTUFF

---
+++ Motor Sensor:Cam/motorCode.py
@@ -0,0 +1,28 @@
+
+from gpiozero import Servo
+from time import sleep
+
+servo = Servo(16)
+val = -1
+
+try:
+	while True:
+    	servo.value = val
+    	sleep(0.1)
+    	val = val + 0.1
+    	if val > 1:
+        	val = -1
+except KeyboardInterrupt:
+	print("Program stopped")
+
+
+try:
+	while True:
+    	servo.min()
+    	sleep(0.5)
+    	servo.mid()
+    	sleep(0.5)
+    	servo.max()
+    	sleep(0.5)
+except KeyboardInterrupt:
+	print("Program stopped")
