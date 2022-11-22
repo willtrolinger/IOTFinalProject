c59a022a804a7bd30c41f06ad2742da077cf994b
William Trolinger <77353770+willtrolinger@users.noreply.github.com>
2022-11-22 17:47:07 -0600

    Add files via upload

---
+++ facial_req.py
@@ -0,0 +1,82 @@
+#! /usr/bin/python
+
+# import the necessary packages
+from imutils.video import VideoStream
+from imutils.video import FPS
+import face_recognition
+import imutils
+import pickle
+import time
+import cv2
+
+currentname = "unknown"
+#Determine faces from encodings.pickle file model created from train_model.py
+encodingsP = "encodings.pickle"
+
+print("[INFO] loading encodings + face detector...")
+data = pickle.loads(open(encodingsP, "rb").read())
+
+# initialize the video stream and allow the camera sensor to warm up
+# Set the src to the followng
+# src = 0 : for the build in single web cam, could be your laptop webcam
+# src = 2 : USB webcam attached to laptop
+vs = VideoStream(src=2,framerate=10).start()
+time.sleep(2.0)
+
+fps = FPS().start()
+
+while True:
+
+	frame = vs.read()
+	frame = imutils.resize(frame, width=500)
+	boxes = face_recognition.face_locations(frame)
+	encodings = face_recognition.face_encodings(frame, boxes)
+	names = []
+
+	for encoding in encodings:
+
+		matches = face_recognition.compare_faces(data["encodings"],
+			encoding)
+		name = "Unknown"
+
+		if True in matches:
+
+			matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+			counts = {}
+
+
+			for i in matchedIdxs:
+				name = data["names"][i]
+				counts[name] = counts.get(name, 0) + 1
+
+
+			name = max(counts, key=counts.get)
+
+			if currentname != name:
+				currentname = name
+				print(currentname)
+
+		names.append(name)
+
+	for ((top, right, bottom, left), name) in zip(boxes, names):
+
+		cv2.rectangle(frame, (left, top), (right, bottom),
+			(0, 255, 225), 2)
+		y = top - 15 if top - 15 > 15 else top + 15
+		cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
+			.8, (0, 255, 255), 2)
+
+	cv2.imshow("Facial Recognition is Running", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	if key == ord("q"):
+		break
+
+	fps.update()
+
+fps.stop()
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+cv2.destroyAllWindows()
+vs.stop()
